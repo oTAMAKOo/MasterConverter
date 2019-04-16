aac7d455b7c4c1db0213cff046597434e3245bd6
junki.hiroi <junki.hiroi@drecom.co.jp>
2019-04-17 00:33:05 +0900

    iniParser を dotnet 実装に変更

--- Source/IniFile.cs
+++ Source/IniFile.cs
@@ -1,58 +1,47 @@
 ﻿
 using System;
 using System.IO;
-using System.Text;
-using System.Runtime.InteropServices;
+using IniParser.Model;
 
 public static class IniFile
 {
-    [DllImport("KERNEL32.DLL")]
-    public static extern uint GetPrivateProfileString(string lpAppName, string lpKeyName, string lpDefault, StringBuilder lpReturnedString, uint nSize, string lpFileName);
-
-    [DllImport("KERNEL32.DLL")]
-    public static extern uint GetPrivateProfileInt(string lpAppName, string lpKeyName, int nDefault, string lpFileName);
-
-    [DllImport("KERNEL32.DLL")]
-    public static extern uint WritePrivateProfileString(string lpAppName, string lpKeyName, string lpString, string lpFileName);
-
     public static T Read<T>(string section, string filepath)
     {
         T ret = (T)Activator.CreateInstance(typeof(T));
 
-        var sb = new StringBuilder(1024);
-
+        var parser = new IniParser.Parser.IniDataParser();
+        IniData config = parser.Parse(File.ReadAllText(filepath));
+        KeyDataCollection sectionData =  config.Sections[section];
+        
         foreach (var n in typeof(T).GetFields())
         {
-            sb.Clear();
-
             if (n.FieldType == typeof(int))
             {
-                n.SetValue(ret, (int)GetPrivateProfileInt(section, n.Name, 0, Path.GetFullPath(filepath)));
-            }
-            else if (n.FieldType == typeof(uint))
-            {
-                n.SetValue(ret, GetPrivateProfileInt(section, n.Name, 0, Path.GetFullPath(filepath)));
+                if (sectionData.ContainsKey(n.Name))
+                {
+                    var value = sectionData[n.Name];
+                    n.SetValue(ret, int.Parse(value));
+                }
             }
             else if (n.FieldType == typeof(bool))
             {
-                GetPrivateProfileString(section, n.Name, "false", sb, (uint)sb.Capacity, Path.GetFullPath(filepath));
-                n.SetValue(ret, bool.Parse(sb.ToString()));
+                if (sectionData.ContainsKey(n.Name))
+                {
+                    var value = sectionData[n.Name];
+                    n.SetValue(ret, bool.Parse(value));
+                }
             }
-            else
+            else if (n.FieldType == typeof(string))
             {
-                GetPrivateProfileString(section, n.Name, "", sb, (uint)sb.Capacity, Path.GetFullPath(filepath));
-                n.SetValue(ret, sb.ToString());
+                if (sectionData.ContainsKey(n.Name))
+                {
+                    var value = sectionData[n.Name];
+                    n.SetValue(ret, value);
+                }
             }
+            
         };
 
         return ret;
-    }
-
-    public static void Write<T>(string secion, T data, string filepath)
-    {
-        foreach (var n in typeof(T).GetFields())
-        {
-            WritePrivateProfileString(secion, n.Name, n.GetValue(data).ToString(), Path.GetFullPath(filepath));
-        };
     }
 }
